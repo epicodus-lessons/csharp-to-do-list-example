4db8857aebfe0e5437467e5f74457bbca4865e09
Courtney Phillips <courtney@epicodus.com>
2018-11-14 09:53:30 -0800

    Add boilerplate for testing file.

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTest
+  {
+
+    // Test methods will go here.
+
+  }
+}
