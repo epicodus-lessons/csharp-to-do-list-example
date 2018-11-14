84d6e84a25a257ecff61bc877ea4b09a12ba7041
Courtney Phillips <courtney@epicodus.com>
2018-11-14 12:15:47 -0800

    Add test confirming SetDescription() overwrites existing Item description properties.

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -28,5 +28,21 @@ namespace ToDoList.Tests
       Assert.AreEqual(description, result);
     }
 
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      //Act
+      string updatedDescription = "Do the dishes";
+      newItem.SetDescription(updatedDescription);
+      string result = newItem.GetDescription();
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
+
   }
 }
