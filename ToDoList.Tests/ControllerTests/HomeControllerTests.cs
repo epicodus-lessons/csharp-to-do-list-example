bf79edde11c355d93475424b1cc82ef4fd31d96d
Courtney Phillips <courtney@epicodus.com>
2018-11-27 15:51:36 -0800

    Add controller test asserting HomeController Index route is correctly returning View.

---
+++ ToDoList.Tests/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using ToDoList.Controllers;
+using ToDoList.Models;
+
+namespace ToDoList.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+
+      [TestMethod]
+      public void Index_ReturnsCorrectView_True()
+      {
+        //Arrange
+        HomeController controller = new HomeController();
+
+        //Act
+        ActionResult indexView = controller.Index();
+
+        //Assert
+        Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+      }
+
+    }
+}
