6c0541733bfaab7df3ab2fe71ac94635a926ce4c
Courtney Phillips <courtney@epicodus.com>
2018-11-28 09:14:35 -0800

    Add test for Find() method.

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -95,5 +95,21 @@ namespace ToDoList.Tests
       Assert.AreEqual(1, result);
     }
 
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+      //Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+
+      //Act
+      Item result = Item.Find(2);
+
+      //Assert
+      Assert.AreEqual(newItem2, result);
+    }
+
   }
 }
