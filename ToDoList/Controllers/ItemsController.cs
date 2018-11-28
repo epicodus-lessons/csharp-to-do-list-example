35c4c8b61b8fd3e191a321c31a9706271676c05e
Courtney Phillips <courtney@epicodus.com>
2018-11-28 09:58:57 -0800

    Add detail view for Items.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -27,6 +27,13 @@ namespace ToDoList.Controllers
       return RedirectToAction("Index");
     }
 
+    [HttpGet("/items/{id}")]
+    public ActionResult Show(int id)
+    {
+      Item item = Item.Find(id);
+      return View(item);
+    }
+
     [HttpPost("/items/delete")]
     public ActionResult DeleteAll()
     {
