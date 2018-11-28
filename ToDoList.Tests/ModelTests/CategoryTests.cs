16614374f37c8afa57824b98252f3bdd7d92c8cb
Courtney Phillips <courtney@epicodus.com>
2018-11-28 11:07:26 -0800

    Add static Find() method to locate Categorys by id.

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -49,5 +49,38 @@ namespace ToDoList.Tests
       Assert.AreEqual(1, result);
     }
 
+    [TestMethod]
+    public void GetAll_ReturnsAllCategoryObjects_CategoryList()
+    {
+      //Arrange
+      string name01 = "Work";
+      string name02 = "School";
+      Category newCategory1 = new Category(name01);
+      Category newCategory2 = new Category(name02);
+      List<Category> newList = new List<Category> { newCategory1, newCategory2 };
+
+      //Act
+      List<Category> result = Category.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectCategory_Category()
+    {
+      //Arrange
+      string name01 = "Work";
+      string name02 = "School";
+      Category newCategory1 = new Category(name01);
+      Category newCategory2 = new Category(name02);
+
+      //Act
+      Category result = Category.Find(2);
+
+      //Assert
+      Assert.AreEqual(newCategory2, result);
+    }
+
   }
 }
