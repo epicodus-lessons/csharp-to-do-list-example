c821a67f774f2241eec775fbe9073f0ee1fa998a
Courtney Phillips <courtney@epicodus.com>
2018-11-28 11:47:35 -0800

    Define new CategoriesController with first Index route.

---
+++ ToDoList/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+
+    [HttpGet("/categories")]
+    public ActionResult Index()
+    {
+      List<Category> allCategories = Category.GetAll();
+      return View(allCategories);
+    }
+
+  }
+}
