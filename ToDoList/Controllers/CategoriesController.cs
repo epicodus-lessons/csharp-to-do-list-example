9fb36baecb2e4bf7a3ff66b34de4055ccd590acc
Courtney Phillips <courtney@epicodus.com>
2018-11-28 13:06:44 -0800

    Create Category Show route and View to display Category detail page.

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -29,5 +29,16 @@ namespace ToDoList.Controllers
       return View("Index", allCategories);
     }
 
+    [HttpGet("/categories/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category selectedCategory = Category.Find(id);
+      List<Item> categoryItems = selectedCategory.GetItems();
+      model.Add("category", selectedCategory);
+      model.Add("items", categoryItems);
+      return View(model);
+    }
+
   }
 }
