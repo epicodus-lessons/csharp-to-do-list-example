4c285ab8bd9177886ca222eb2c13c430656d7619
Courtney Phillips <courtney@epicodus.com>
2018-11-28 11:09:23 -0800

    Define Find() method in Category class, tests pass.

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -8,5 +8,39 @@ namespace ToDoList.Models
     private string _name;
     private int _id;
     private List<Item> _items;
+
+    public Category(string categoryName)
+    {
+      _name = categoryName;
+      _instances.Add(this);
+      _id = _instances.Count;
+      _items = new List<Item>{};
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Category> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Category Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
   }
 }
