1d4551127e06c607dbafde14f7c840959305131c
Courtney Phillips <courtney@epicodus.com>
2018-11-29 12:11:46 -0800

    Add database configuration class and connection string to Startup.cs.

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -39,4 +39,10 @@ namespace ToDoList
 
     }
   }
+
+  public static class DBConfiguration
+  {
+    public static string ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=to_do_list;";
+  }
+
 }
