fe8269276f5858881b78cbb0e889b037085642cf
Courtney Phillips <courtney@epicodus.com>
2018-11-29 13:36:33 -0800

    Comment out unused areas of code still needing refactor.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,14 +5,12 @@ namespace ToDoList.Models
   public class Item
   {
     private string _description;
-    private int _id;
-    private static List<Item> _instances = new List<Item> {};
+    // private int _id;
 
     public Item (string description)
     {
       _description = description;
-      _instances.Add(this);
-      _id = _instances.Count;
+      // _id = _instances.Count;
     }
 
     public string GetDescription()
@@ -25,25 +23,43 @@ namespace ToDoList.Models
       _description = newDescription;
     }
 
-    public int GetId()
-    {
-      return _id;
-    }
+    // public int GetId()
+    // {
+    //   return _id;
+    // }
 
     public static List<Item> GetAll()
     {
-      return _instances;
+      List<Item> allItems = new List<Item> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int itemId = rdr.GetInt32(0);
+        string itemDescription = rdr.GetString(1);
+        Item newItem = new Item(itemDescription, itemId);
+        allItems.Add(newItem);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allItems;
     }
 
-    public static void ClearAll()
-    {
-      _instances.Clear();
-    }
+    // public static void ClearAll()
+    // {
+    //   _instances.Clear();
+    // }
 
-    public static Item Find(int searchId)
-    {
-      return _instances[searchId-1];
-    }
+    // public static Item Find(int searchId)
+    // {
+    //   return _instances[searchId-1];
+    // }
 
   }
 }
