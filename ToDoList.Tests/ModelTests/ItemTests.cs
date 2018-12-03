fb7e28765193cb8e1c5b490e8632e95b26d786d0
Courtney Phillips <courtney@epicodus.com>
2018-12-03 10:49:02 -0800

    Add connection string to test database in ItemTests class.

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -14,6 +14,11 @@ namespace ToDoList.Tests
       Item.ClearAll();
     }
 
+    public ItemTests()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=to_do_list_test;";
+    }
+
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
