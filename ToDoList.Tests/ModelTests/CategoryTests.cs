fd264cea0eba549be094f4475ce5697a825afa4d
Courtney Phillips <courtney@epicodus.com>
2018-12-04 15:38:07 -0800

    Refactor previous test for GetAll() to use new Save() method.

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -53,24 +53,26 @@ namespace ToDoList.Tests
     //   //Assert
     //   Assert.AreEqual(1, result);
     // }
-    //
-    // [TestMethod]
-    // public void GetAll_ReturnsAllCategoryObjects_CategoryList()
-    // {
-    //   //Arrange
-    //   string name01 = "Work";
-    //   string name02 = "School";
-    //   Category newCategory1 = new Category(name01);
-    //   Category newCategory2 = new Category(name02);
-    //   List<Category> newList = new List<Category> { newCategory1, newCategory2 };
-    //
-    //   //Act
-    //   List<Category> result = Category.GetAll();
-    //
-    //   //Assert
-    //   CollectionAssert.AreEqual(newList, result);
-    // }
-    //
+
+    [TestMethod]
+    public void GetAll_ReturnsAllCategoryObjects_CategoryList()
+    {
+      //Arrange
+      string name01 = "Work";
+      string name02 = "School";
+      Category newCategory1 = new Category(name01);
+      newCategory1.Save();
+      Category newCategory2 = new Category(name02);
+      newCategory2.Save();
+      List<Category> newList = new List<Category> { newCategory1, newCategory2 };
+
+      //Act
+      List<Category> result = Category.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
     // [TestMethod]
     // public void Find_ReturnsCorrectCategory_Category()
     // {
