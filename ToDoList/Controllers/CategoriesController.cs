8e62c9ccf48d49d83690de3cf7e59ea1074717a2
Courtney Phillips <courtney@epicodus.com>
2018-12-04 11:42:25 -0800

    Add missing call to Save() in Item creation route of CategoriesController.

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -47,6 +47,7 @@ namespace ToDoList.Controllers
       Dictionary<string, object> model = new Dictionary<string, object>();
       Category foundCategory = Category.Find(categoryId);
       Item newItem = new Item(itemDescription);
+      newItem.Save();
       foundCategory.AddItem(newItem);
       List<Item> categoryItems = foundCategory.GetItems();
       model.Add("items", categoryItems);
