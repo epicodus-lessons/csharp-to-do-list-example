d7b1c07a03a8638e137daee081ada4287de377d2
Courtney Phillips <courtney@epicodus.com>
2018-12-04 15:13:10 -0800

    Connect CategoryTest class to test database in constructor."

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -9,6 +9,11 @@ namespace ToDoList.Tests
   public class CategoryTest : IDisposable
   {
 
+    public CategoryTest()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=to_do_list_test;";
+    }
+
     public void Dispose()
     {
       Category.ClearAll();
