c72eb67ae2913c02265ac5863ad04fee7298905b
Courtney Phillips <courtney@epicodus.com>
2018-12-04 10:10:20 -0800

    Add edit functionality in Item class.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -129,6 +129,25 @@ namespace ToDoList.Models
 
     public void Edit(string newDescription)
     {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"UPDATE items SET description = @newDescription WHERE id = @searchId;";
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = _id;
+      cmd.Parameters.Add(searchId);
+      MySqlParameter description = new MySqlParameter();
+      description.ParameterName = "@newDescription";
+      description.Value = newDescription;
+      cmd.Parameters.Add(description);
+      cmd.ExecuteNonQuery();
+      _description = newDescription; // <--- This line is new!
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
     }
 
   }
