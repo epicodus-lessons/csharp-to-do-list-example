36117fb5fd07b022714fc1563008d1f7bca88a31
Courtney Phillips <courtney@epicodus.com>
2018-12-04 15:20:14 -0800

    Update Category GetAll() method to query database.

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -49,7 +49,25 @@ namespace ToDoList.Models
 
     public static List<Category> GetAll()
     {
-      return _instances;
+      List<Category> allCategories = new List<Category> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM categories;";
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int CategoryId = rdr.GetInt32(0);
+        string CategoryName = rdr.GetString(1);
+        Category newCategory = new Category(CategoryName);
+        allCategories.Add(newCategory);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allCategories;
     }
 
     public static Category Find(int searchId)
