9f4e164f556ac2d06f7f674785fb37f36c93d7bf
Courtney Phillips <courtney@epicodus.com>
2018-12-06 11:45:59 -0800

    Comment out Category GetItems() method to get beyond compiler errors to follow BDD in Item class refactor.

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -88,33 +88,33 @@ namespace ToDoList.Models
       return newCategory;
     }
 
-    public List<Item> GetItems()
-    {
-      List<Item> allCategoryItems = new List<Item> {};
-      MySqlConnection conn = DB.Connection();
-      conn.Open();
-      var cmd = conn.CreateCommand() as MySqlCommand;
-      cmd.CommandText = @"SELECT * FROM items WHERE category_id = @category_id;";
-      MySqlParameter categoryId = new MySqlParameter();
-      categoryId.ParameterName = "@category_id";
-      categoryId.Value = this._id;
-      cmd.Parameters.Add(categoryId);
-      var rdr = cmd.ExecuteReader() as MySqlDataReader;
-      while(rdr.Read())
-      {
-        int itemId = rdr.GetInt32(0);
-        string itemDescription = rdr.GetString(1);
-        int itemCategoryId = rdr.GetInt32(2);
-        Item newItem = new Item(itemDescription, itemCategoryId, itemId);
-        allCategoryItems.Add(newItem);
-      }
-      conn.Close();
-      if (conn != null)
-      {
-        conn.Dispose();
-      }
-      return allCategoryItems;
-    }
+    // public List<Item> GetItems()
+    // {
+    //   List<Item> allCategoryItems = new List<Item> {};
+    //   MySqlConnection conn = DB.Connection();
+    //   conn.Open();
+    //   var cmd = conn.CreateCommand() as MySqlCommand;
+    //   cmd.CommandText = @"SELECT * FROM items WHERE category_id = @category_id;";
+    //   MySqlParameter categoryId = new MySqlParameter();
+    //   categoryId.ParameterName = "@category_id";
+    //   categoryId.Value = this._id;
+    //   cmd.Parameters.Add(categoryId);
+    //   var rdr = cmd.ExecuteReader() as MySqlDataReader;
+    //   while(rdr.Read())
+    //   {
+    //     int itemId = rdr.GetInt32(0);
+    //     string itemDescription = rdr.GetString(1);
+    //     int itemCategoryId = rdr.GetInt32(2);
+    //     Item newItem = new Item(itemDescription, itemCategoryId, itemId);
+    //     allCategoryItems.Add(newItem);
+    //   }
+    //   conn.Close();
+    //   if (conn != null)
+    //   {
+    //     conn.Dispose();
+    //   }
+    //   return allCategoryItems;
+    // }
 
     public override bool Equals(System.Object otherCategory)
     {
