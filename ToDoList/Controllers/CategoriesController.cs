d9da1bb941357d7fc371534db0c423b60f63fb90
Courtney Phillips <courtney@epicodus.com>
2018-12-10 10:19:05 -0800

    Remove route from CategoriesController previously responsible for creating new Items."

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -39,21 +39,6 @@ namespace ToDoList.Controllers
     //   model.Add("items", categoryItems);
     //   return View(model);
     // }
-    //
-    // // This one creates new Items within a given Category, not new Categories:
-    // [HttpPost("/categories/{categoryId}/items")]
-    // public ActionResult Create(int categoryId, string itemDescription)
-    // {
-    //   Dictionary<string, object> model = new Dictionary<string, object>();
-    //   Category foundCategory = Category.Find(categoryId);
-    //   Item newItem = new Item(itemDescription);
-    //   newItem.Save();
-    //   foundCategory.AddItem(newItem);
-    //   List<Item> categoryItems = foundCategory.GetItems();
-    //   model.Add("items", categoryItems);
-    //   model.Add("category", foundCategory);
-    //   return View("Show", model);
-    // }
 
   }
 }
